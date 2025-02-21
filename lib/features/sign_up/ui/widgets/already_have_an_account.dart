71e741d8db829129a1518eb1cd2f4174e28a7a2f
Kareem Hamdy Mohamed <kareemkhdh543@gmail.com>
2025-02-22 00:13:31 +0200

    Resolved merge conflict in already_have_an_account.dart

--- lib/features/sign_up/ui/widgets/already_have_an_account.dart
+++ lib/features/sign_up/ui/widgets/already_have_an_account.dart
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-import 'package:flutter/material.dart';
-=======
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_course/core/helpers/extensions.dart';
 import 'package:flutter_advanced_course/core/routing/routes.dart';
->>>>>>> b9a88554947e6a9820ce828d20d262de7d82e4f2
 import 'package:flutter_advanced_course/core/theme/styles.dart';
 
 class AlreadyHaveAccountText extends StatelessWidget {
@@ -17,19 +13,16 @@ class AlreadyHaveAccountText extends StatelessWidget {
       textAlign: TextAlign.center,
       text: TextSpan(children: [
         TextSpan(
-          text: 'Already have an accout?',
+          text: 'Already have an account?',
           style: Styles.font14GreyRegular,
         ),
         TextSpan(
           text: ' Sign In ',
           style: Styles.font14BlueSemiBold,
-<<<<<<< HEAD
-=======
           recognizer: TapGestureRecognizer()
             ..onTap = () {
               context.pushNamed(Routes.loginScreen);
             },
->>>>>>> b9a88554947e6a9820ce828d20d262de7d82e4f2
         ),
       ]),
     );
