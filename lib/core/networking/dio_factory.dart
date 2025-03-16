2267609b687d40d5eb2f3b36f2c673ad6daf3b23
Kareem Hamdy Mohamed <kareemkhdh543@gmail.com>
2025-03-16 15:34:11 +0200

    - fixed getSecuredString in the dio factory

--- lib/core/networking/dio_factory.dart
+++ lib/core/networking/dio_factory.dart
@@ -28,7 +28,7 @@ class DioFactory {
     dio?.options.headers = {
       'Accept': 'application/json',
       'Authorization':
-          'Bearer ${await SharedPrefHelper.getString(SharedPrefKeys.userToken)}'
+          'Bearer ${await SharedPrefHelper.getSecuredString(SharedPrefKeys.userToken)}'
     };
   }
 
